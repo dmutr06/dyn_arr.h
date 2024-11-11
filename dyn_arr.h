d9bf6e7b6f706e4ee283b18bb3778c7556abddc3
dmutro <shaduraa.dmutroo@gmail.com>
2024-11-11 20:18:07 +0200

    initial commit

---
+++ dyn_arr.h
@@ -0,0 +1,66 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef DYN_ARR_H_
+#define DYN_ARR_H_
+
+static const size_t INIT_CAP = 16;
+
+static void default_deinit(void *item) {}
+
+#define DynArr(type...) \
+  struct { \
+    size_t size; \
+    size_t cap; \
+    type *items; \
+    void (*deinit_item)(type *item); \
+  }
+
+#define darr_init(type, deinit) { \
+    .cap = INIT_CAP, \
+    .size = 0, \
+    .items = malloc(INIT_CAP * sizeof(type)), \
+    .deinit_item = deinit ? deinit : (void (*)(type *)) default_deinit \
+}
+
+#define darr_push(arr, item...) { \
+  if ((arr)->size >= (arr)->cap) { \
+    (arr)->cap *= 2; \
+    (arr)->items = realloc((arr)->items, (arr)->cap * sizeof(*(arr)->items)); \
+  } \
+  (arr)->items[(arr)->size++] = item; \
+} (arr)->size
+
+#define darr_get(arr, idx...) (((arr)->size > (idx) || (idx) < 0) ? ((arr)->items + (idx)) : NULL)
+
+#define darr_deinit(arr) do { \
+  if ((void (*)(void *))(arr)->deinit_item != default_deinit) \
+    darr_foreach(arr, item) (arr)->deinit_item(item); \
+  free((arr)->items); \
+} while (0) \
+
+#define darr_pop(arr) do { \
+  if ((arr)->size <= 0) break; \
+  (arr)->deinit_item((arr)->items + --(arr)->size); \
+} while (0); (arr)->size 
+
+#define darr_remove(arr, idx...) do { \
+  if ((idx) < 0) break; \
+  if ((idx) == (arr)->size - 1) { \
+    darr_pop(arr); \
+    break; \
+  } \
+  if ((idx) >= (arr)->size) break; \
+  (arr)->deinit_item((arr)->items + (idx)); \
+  memmove((arr)->items + (idx), (arr)->items + (idx) + 1, (--(arr)->size - (idx)) * sizeof(*(arr)->items)); \
+} while (0); (arr)->size
+
+#define darr_first(arr) darr_get(arr, 0)
+
+#define darr_last(arr) darr_get(arr, (arr)->size - 1)
+
+#define darr_foreach(arr, item) \
+  for (typeof(*((arr)->items)) *item = (arr)->items; item < (arr)->items + (arr)->size; item++)
+
+#endif // DYN_ARR_H_
---
+++ example.c
@@ -0,0 +1,44 @@
+#include "dyn_arr.h"
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct {
+  float x;
+  float y;
+} Point;
+
+typedef DynArr(Point) PointArr;
+
+void deinit_point(Point *p) {
+  printf("Goodbye, (%g, %g)\n", p->x, p->y);
+}
+
+bool point_in_arr(PointArr *arr, Point p) {
+  darr_foreach(arr, item)
+    if (item->x == p.x && item->y == p.y) return true;
+
+  return false;
+}
+
+void print_arr(PointArr *arr) {
+  darr_foreach(arr, p)
+    printf("(%g, %g)\n", p->x, p->y);
+}
+
+int main() {
+  PointArr arr = darr_init(Point, deinit_point);
+  darr_push(&arr, (Point) { 2.5, 2.5 });
+  darr_push(&arr, (Point) { 3, 1.0 });
+  darr_push(&arr, (Point) { 0.0, 0.5 });
+  
+  printf("---------\n");
+  print_arr(&arr);
+  printf("---------\n");
+
+  if (point_in_arr(&arr, (Point) { 3, 1 }))
+    printf("WOW, point (3, 1) is in the array!\n");
+
+  printf("---------\n");
+  darr_deinit(&arr);
+  return 0;
+}
